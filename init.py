fc740205ca5b17686b6d4b46912c135613976607
jilanimokrani <mokranijilani@gmail.com>
2021-01-15 13:52:48 +0100

    VGG Transfer Learning Environment

--- init.py
+++ init.py
@@ -1,6 +1,5 @@
 import tensorflow.keras.applications.vgg16 as vgg16
 import tensorflow.keras.applications.vgg19 as vgg19
 
-model = vgg16.VGG16()
-print("hello")
-model = vgg19.VGG19()
+model = vgg16.VGG16(weights= "imagenet")
+model = vgg19.VGG19(weights= "imagenet")
