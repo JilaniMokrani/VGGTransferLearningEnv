b410610da9cc0e6b4744d0bc7161bf034f67def3
JilaniMokrani <root@pop-os.localdomain>
2021-01-15 12:22:29 +0100

    Training Environment

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM tensorflow/tensorflow:2.3.0
 LABEL MAINTAINER="Jilani Mokrani"
 COPY ./*.py ./
 RUN python3 ./init.py
+
--- init.py
+++ init.py
@@ -2,4 +2,5 @@ import tensorflow.keras.applications.vgg16 as vgg16
 import tensorflow.keras.applications.vgg19 as vgg19
 
 model = vgg16.VGG16()
+print("hello")
 model = vgg19.VGG19()
