eea1f67689e095050e2e5ff046f603202c69a76c
jilanimokrani <mokranijilani@gmail.com>
2021-01-18 11:17:55 +0100

    Rectifications

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 import tensorflow.keras.applications.vgg16 as vgg16
 import tensorflow.keras.applications.vgg19 as vgg19
 
-model = vgg16.VGG16(weights= "imagenet")
-model = vgg19.VGG19(weights= "imagenet")
+model = vgg16.VGG16(weights= "imagenet", include_top=false)
+model = vgg19.VGG19(weights= "imagenet", include_top= false)
